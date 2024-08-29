0a6bf80d329ac32f6871858d60a8ca9976ddab88
bill donner <billdonner@gmail.com>
2024-08-29 10:34:48 -0400

    Initial Commit

---
+++ qats/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  qats
+//
+//  Created by bill donner on 8/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ qats/qatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  qatsApp.swift
+//  qats
+//
+//  Created by bill donner on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct qatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
